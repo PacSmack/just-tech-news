b2714c24d7fac3b9a3dd5eb9df9e43a15f0f3269
Nathan da Silva <pacsmacck@gmail.com>
2021-05-31 01:54:53 -0700

    added bcrypt for security improvement

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create our User model
-class User extends Model {}
+class User extends Model { }
 
 //define table columns and configuration
 User.init(
@@ -42,7 +43,18 @@ User.init(
     },
     {
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+        hooks: {
+            //  set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData;
+            },
 
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10)
+                return updatedUserData
+            }
+        },
         //pass in our imported sequelize connection (direct connection to our database)
         sequelize,
         //dont automatically create createdAt/updatedAt timestamp fields
@@ -53,7 +65,7 @@ User.init(
         underscored: true,
         //make it so our model name stays lowercase in the database
         modelName: 'user'
-    }
+    },
 );
 
 module.exports = User;
